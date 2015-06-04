92fdcdd2aa10e9a1963d9b461646af5dbf457708
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-06-04 11:53:09 -0400

    placing multiple, different colored markers on map

--- app/assets/javascripts/regions.js
+++ app/assets/javascripts/regions.js
@@ -12,17 +12,45 @@ function initRegions() {
     map.fitBounds(results[0].geometry.viewport);
   });
 
-  var myLatlng = new google.maps.LatLng(33.7205771, -116.2155619);
+  locations = [
+    ['Coachella', 33.7205771, -116.2155619],
+    ['Bonnaroo', 35.4817431, -86.0885993],
+    ['Lollapalooza', 41.8781136, -87.6297982],
+    ['Ultra', 25.7616798, -80.1917902],
+    ['Governors Ball', 40.7127837, -74.0059413]
+  ];
 
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-  });
+  var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
 
-  marker.setMap(map);
+  var icons = [
+    iconURLPrefix + 'red-dot.png',
+    iconURLPrefix + 'green-dot.png',
+    iconURLPrefix + 'blue-dot.png',
+    iconURLPrefix + 'orange-dot.png',
+    iconURLPrefix + 'yellow-dot.png'
+  ];
 
-  var infowindow = new google.maps.InfoWindow({
-  content: "Coachella"
-  });
+  var iconsLength = icons.length;
+  var infowindow = new google.maps.InfoWindow({ maxWidth: 160 });
+  var markers = new Array();
+  var iconCounter = 0;
 
-  infowindow.open(map,marker);
+  for (var i = 0; i < locations.length; i++)  {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map,
+      icon: icons[iconCounter]
+    });
+    markers.push(marker);
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+    iconCounter++;
+    if(iconCounter >= iconsLength) {
+      iconCounter = 0;
+    }
+  }
 }
